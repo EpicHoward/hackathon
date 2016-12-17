79ee3d586a29cfd64c8ada8f4ff1a43848ef87f0
Eric R. Howard <ehoward@hudsonhs.com>
2016-12-17 18:14:16 +0000

    commit

--- java.js
+++ java.js
@@ -1,3 +1,42 @@
 // JavaScript File
+$(document).ready(function(){
+    
+    var genTrash = function(){
+      var div = $("<div></div>");
+      div.addClass('melon');
+      div.addClass('trash');
+      div.css({top: 200, left: 200, });
+      $("#trashspace").append(div);
+    };
+    //var imgGenerator = Math.floor(Math.random() * 1000px);
+    var genRandom = function(num){
+      return Math.floor(Math.random()*num );
+    };
+    
+    genTrash();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
 
-
+   
